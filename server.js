9cdba55b3b5b4010b9462e944ebdbfacf261fcf5
= <danjavia.iconshock@gmail.com>
2014-12-12 11:26:24 -0500

    initial commit 2

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
+var cf = { user: "heroku_app32451573", account: "heroku_app32451573" }
 // require deployd
-var deployd = require('deployd');
+var deployd = require( 'deployd' );
 // configure database etc.
 var server = deployd({
 	port: process.env.PORT || 5000,
 	env: 'production',
 	db: {
-		host: 'host.mongolab.com',
-		port: 33559,
-		name: 'heroku_app',
+		host: 'ds063170.mongolab.com',
+		port: 63170,
+		name: 'heroku_app32451573',
 		credentials: {
-			username: 'heroku_app',
-			password: 'password'
+			username: 'heroku_app32451573',
+			password: '6lkdpl6vj9ck1u5eu15p9tppjt'
 		}
 	}
 });
 
 // heroku requires these settings for sockets to work
-server.sockets.manager.settings.transports = ["xhr-polling"];
+server.sockets.manager.settings.transports = [ "xhr-polling" ];
 
 // start the server
 server.listen();
 
 // debug
-server.on('listening', function() {
-	console.log("Server is listening on port: " + process.env.PORT);
+server.on( 'listening', function() {
+	console.log( 'Server is listening on port: ' + process.env.PORT );
 });
 
 // Deployd requires this
-server.on('error', function(err) {
-	console.error(err);
-	process.nextTick(function() { // Give the server a chance to return an error
+server.on( 'error', function( err ) {
+	console.error( err );
+	process.nextTick( function() { // Give the server a chance to return an error
 		process.exit();
 	});
 });
